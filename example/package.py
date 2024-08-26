bb0ca5f72ba4e4570c50fb180f56d342e9d31ec5
Zeke Zhang <sean1832725142@gmail.com>
2024-08-26 18:12:25 +1000

    doc: update example file

--- example/package.py
+++ example/package.py
@@ -16,13 +16,25 @@ def main():
     solution_root = os.path.dirname(os.path.dirname(project_file))
     example_file = os.path.join(solution_root, "Example/grasshopper", "portal-example.gh")
 
-    bd = builder.AssemblyBuilder(project_file)
-    bd.build_zip(
-        zip_filename=f"{bd.project_name}_v{bd.version}.zip",
+    proj = builder.AssemblyBuilder(project_file)
+    proj.build_zip(
+        input_dir = os.path.join(proj.project_dir, "bin", "release", "net48"),
+        zip_filename=f"{proj.project_name}-{proj.version}.zip",
         exclude_patterns=["RhinoCommon.dll", "Grasshopper.dll", "*.xml", "*.pdb"],
         include_files=["LICENSE", "README.md"],
     )
-    bd.package_example(example_file)
+    proj.build_folder(
+        dir_paths=[os.path.join(solution_root, "Example")],
+        folder_name="examples",
+        exclude_patterns=["*.gh", "*.3dm"],
+        include_files=["LICENSE", "README.md", "icon.png", "manifest.json"],
+    )
+
+    proj.copy_file(
+        example_file,
+        os.path.join(proj.output_folder, "examples"),
+        rename=f"{proj.project_name}-example-{proj.version}.gh" # optional
+    )
 
 
 if __name__ == "__main__":
